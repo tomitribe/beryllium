b33f83f1040e13ae79eabc6edfb1be9580c52b05
Carlos Chacin <cchacin@gmail.com>
2015-12-04 00:43:58 -0800

    Cleaning CucumberOptions

--- src/test/java/org/tomitribe/beryllium/rest/RestfulSuccessTest.java
+++ src/test/java/org/tomitribe/beryllium/rest/RestfulSuccessTest.java
@@ -1,20 +1,14 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package org.tomitribe.beryllium.rest;
 
@@ -24,21 +18,17 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.runner.RunWith;
-import org.tomitribe.beryllium.CallsSteps;
-import org.tomitribe.beryllium.DatabaseSteps;
-import org.tomitribe.beryllium.RedisKeyValueSteps;
-import org.tomitribe.beryllium.RedisListSteps;
-import org.tomitribe.beryllium.RedisScoredMembersSteps;
-import org.tomitribe.beryllium.RedisSteps;
-import org.tomitribe.beryllium.RestSteps;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import cucumber.runtime.arquillian.ArquillianCucumber;
-import cucumber.runtime.arquillian.api.Features;
-import cucumber.runtime.arquillian.api.Glues;
 
-@Glues({RestSteps.class, DatabaseSteps.class, CallsSteps.class, RedisKeyValueSteps.class,
-               RedisListSteps.class, RedisScoredMembersSteps.class, RedisSteps.class})
-@Features({"features/successful-endpoints.feature", "features/redis.feature"})
+@CucumberOptions(snippets = SnippetType.CAMELCASE,
+                 strict = true,
+                 glue = {"classpath:"},
+                 features = {"classpath:features"},
+                 plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"}
+)
 @RunWith(ArquillianCucumber.class)
 public class RestfulSuccessTest {
 
