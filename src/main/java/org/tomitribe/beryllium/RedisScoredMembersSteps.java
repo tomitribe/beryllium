21fd4764ae1f14ea0939d3b34c433dfb825a8012
Carlos Chacin <cchacin@gmail.com>
2015-12-09 00:56:06 -0800

    Using JedisPool

--- src/main/java/org/tomitribe/beryllium/RedisScoredMembersSteps.java
+++ src/main/java/org/tomitribe/beryllium/RedisScoredMembersSteps.java
@@ -24,11 +24,22 @@ import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import static com.google.common.truth.Truth.assertThat;
 
 public class RedisScoredMembersSteps {
-    private final Jedis jedis = new Jedis("localhost");
+    private final JedisPool jedisPool = new JedisPool("localhost");
+
+    private Jedis getJedis(final Integer db) {
+        final Jedis jedis = jedisPool.getResource();
+        if (db != null) {
+            jedis.select(db);
+        } else {
+            jedis.select(0);
+        }
+        return jedis;
+    }
 
     @Given("^I have the redis scored member \"([^\"]*)\"(?: in the db (\\d+))? with score \"([^\"]*)\" and value \"([^\"]*)\"$")
     public void iHaveTheRedisScoredMemberInTheDbWithScoreAndValue(final String key,
@@ -36,33 +47,33 @@ public class RedisScoredMembersSteps {
                                                                   final String score,
                                                                   final String value) {
         final double scoreValue = Double.parseDouble(score);
-        jedis.select(database);
-        jedis.zadd(key, scoreValue, value);
+        getJedis(database).select(database);
+        getJedis(database).zadd(key, scoreValue, value);
     }
 
     @Given("^I have the redis scored members \"([^\"]*)\"(?: in the db (\\d+))? with values:$")
     public void iHaveTheRedisScoredMembersInTheDbWithValuesColon(final String key,
                                                                  final int database, final DataTable dataTable) {
         final Map<String, Double> table = dataTable.asMap(String.class, Double.class);
-        jedis.select(database);
-        jedis.zadd(key, table);
+        getJedis(database).select(database);
+        getJedis(database).zadd(key, table);
     }
 
     @Then("^I should have the redis scored member \"([^\"]*)\"(?: in the db (\\d+))? with score \"([^\"]*)\" and value \"([^\"]*)\"$")
     public void iShouldHaveTheRedisScoredMemberInTheDbWithScoreAndValue(
             final String key, final int database, final String score, final String value) {
         final double scoredValue = Double.parseDouble(score);
-        jedis.select(database);
-        assertThat(jedis.zscore(key, value)).isEqualTo(scoredValue);
+        getJedis(database).select(database);
+        assertThat(getJedis(database).zscore(key, value)).isEqualTo(scoredValue);
     }
 
     @Then("^I should have the redis scored members \"([^\"]*)\"(?: in the db (\\d+))? with values:$")
     public void iShouldHaveTheRedisScoredMembersInTheDbWithValuesColon(final String key,
                                                                        final int database, final DataTable dataTable) {
         final Map<String, Double> table = dataTable.asMap(String.class, Double.class);
-        jedis.select(database);
+        getJedis(database).select(database);
         for (Map.Entry<String, Double> entry : table.entrySet()) {
-            assertThat(jedis.zscore(key, entry.getKey())).isEqualTo(entry.getValue());
+            assertThat(getJedis(database).zscore(key, entry.getKey())).isEqualTo(entry.getValue());
         }
     }
 }
